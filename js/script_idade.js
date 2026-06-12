ba79a4a81e185d3e91f58656fa7089bd80dfbbbf
Lucas Azedi <lucas.a.goes@aluno.senai.br>
2026-06-12 15:07:17 -0300

    Colocando a idade e resultado

--- js/script_idade.js
+++ js/script_idade.js
@@ -1,3 +1,22 @@
 //PEGANDO ELEMENTOS DO DOM
 const formDados = document.querySelector('#formulario-pessoa')
 const divResultado = document.querySeklector('#div-dados')
+
+//CAPTURANDO O EVENTO SUBMIT DO FORMULÁRIO
+formDados.addEventListener('submit', (evt)=>{
+    evt.preventDefault()
+
+    const objFormDados = new FormData(formDados)
+
+    let nome = objFormDados.get('nome')
+    let idade = objFormDados.get('idade')
+
+    let situacaoIdade = ''
+
+    if (idade >= 18){
+        situacaoIdade = `${nome}, você é maior de idade`
+    }else{
+        situacaoIdade = `${nome}, você é menor de idade`
+    } 
+    
+})
