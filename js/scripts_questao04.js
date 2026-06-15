1f8059fef5f146f96b62c8693502b4b960abe1be
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-06-15 14:57:35 -0300

    Scripts da Questão 04

---
+++ js/scripts_questao04.js
@@ -0,0 +1,24 @@
+//PEGANDO ELEMENTOS DO DOM
+const formDados = document.querySelector('#formulario')
+const divResultado = document.querySelector('#div-dados')
+
+//CAPTURANDO O EVENTO SUBMIT DO FORMULÁRIO
+formDados.addEventListener('submit', (evt)=>{
+    evt.preventDefault()
+
+    const objFormDados = new FormData(formDados)
+
+    let distancia_pecorrida = parseFloat(objFormDados.get('distancia_pecorrida'))
+    let consumo = parseFloat(objFormDados.get('consumo'))
+     let preço = parseFloat(objFormDados.get('preço'))
+
+   let pecorrido = distancia_pecorrida / consumo
+   let gasto = preço * pecorrido
+
+
+    divResultado.innerHTML = `A quantidade de combustível necessário para percorrer o trajeto: ${pecorrido.toFixed(2).replace('.',',')}KM, O valor total a pagar com combustível para esse
+deslocamento.R$ ${gasto.toFixed(2).replace('.',',')}`
+
+    formDados.reset()
+    
+})
